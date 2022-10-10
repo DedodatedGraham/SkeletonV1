50c01c4d6f8e0a63c44fe02ed4bd59d30fd6ef09
Graham_Garcia1 <grahamg7584@gmail.com>
2022-10-10 10:32:18 -0500

    quickcommit

--- Src/DataStruct/kdgpu.cu
+++ Src/DataStruct/kdgpu.cu
@@ -2,7 +2,13 @@
 //Includes Make Kernals, Search Kernals, and Deviation Kernals
 #include <stdio.h>
 #include "kdgpu.cuh"
+#include "ops/lock.h"
 
-__global__ void buildKD(){
-        
+
+__global__ void buildKD(Lock lock){
+
+}
+
+__global__ void searchKD(Lock lock){
+
 }
