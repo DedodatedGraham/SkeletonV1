726043dc337d67dc023fbf390e1549c4473a45af
DedodatedGraham <=>
2022-11-17 21:01:31 -0600

    quickcommit

--- Supercomputer/3DFIG.sh
+++ Supercomputer/3DFIG.sh
@@ -9,7 +9,7 @@ echo "node file $PBS_NODEFILE"
 num=`cat $PBS_NODEFILE | wc -l`
 
 mpiexec -n 1 -machinefile $PBS_NODEFILE python3 ../Src/Load.py 
-mpiexec -n 1 -machinefile $PBS_NODEFILE python3 ../Src/3DSpin.py -i 'Output/SkeleSave.dat' -n 30 -m 0
+mpiexec -n 1 -machinefile $PBS_NODEFILE python3 ../Src/3DSpin.py -i 'Output' -n 30 -m 0
 mpiexec -n 1 -machinefile $PBS_NODEFILE ffmpeg -r 72 -y -threads 4 -i ../AnimationData/Spin/%03dspin.png -pix_fmt yuv420p ../AnimationData/Spin/Spin.mp4 
 
 
